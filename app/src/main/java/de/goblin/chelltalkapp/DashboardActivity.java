82f7e734c0be150d8662bdafdef6d32e890523bf
Goblin <mc.goblin@t-online.de>
2023-07-12 23:55:37 +0200

    changed Dashboard:selectDevice stlye added drop_down_item.xml added custom_scrollbar.xml

--- app/src/main/java/de/goblin/chelltalkapp/DashboardActivity.java
+++ app/src/main/java/de/goblin/chelltalkapp/DashboardActivity.java
@@ -13,8 +13,10 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -24,17 +26,25 @@ public class DashboardActivity extends AppCompatActivity {
     private BluetoothManager BTManager;
     private BluetoothAdapter BTAdapter;
     private Set<BluetoothDevice> pairedDevices;
-    Spinner sp_pairedDevices;
+    AutoCompleteTextView acTextView_pairedDevices;
+
+
+    private ArrayList deviceList = new ArrayList();
+    private ArrayAdapter<String> adapter_deviceList;
+    private String selectedDevice = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        acTextView_pairedDevices = (AutoCompleteTextView) findViewById(R.id.autocompletetextview_pairedDevices);
+
+        adapter_deviceList = new ArrayAdapter(this, R.layout.drop_down_item, deviceList);
+
         BTManager = (BluetoothManager) this.getSystemService(Context.BLUETOOTH_SERVICE);
         BTAdapter = BTManager.getAdapter();
         checkBluetoothPermisson();
-        sp_pairedDevices = (Spinner) findViewById(R.id.spinner_pairedDevices);
         deviceList();
     }
 
@@ -56,22 +66,16 @@ public class DashboardActivity extends AppCompatActivity {
 
     @SuppressLint("MissingPermission")
     public void deviceList() {
-        ArrayList deviceList = new ArrayList();
-        deviceList.add("Select device");
         checkBluetoothPermisson();
         pairedDevices = BTAdapter.getBondedDevices();
 
         if (pairedDevices.size() < 1) {
             Toast.makeText(getApplicationContext(), "No paired devices found", Toast.LENGTH_SHORT).show();
-            final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, deviceList);
-            sp_pairedDevices.setAdapter(adapter);
-            sp_pairedDevices.setSelection(0);
         }
         else {
             for (BluetoothDevice bt : pairedDevices) deviceList.add(bt.getName() + " " + bt.getAddress());
-            final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, deviceList);
-            sp_pairedDevices.setAdapter(adapter);
-            sp_pairedDevices.setSelection(0);
+            acTextView_pairedDevices.setAdapter(adapter_deviceList);
+            acTextView_pairedDevices.setSelection(0);
         }
     }
 }
